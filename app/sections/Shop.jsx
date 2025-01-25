7f4fbc89e3b2f8afd7a00d54045c1e97fd85a6fa
Jason Sugiharto <jasonliesugiharto@gmail.com>
2025-01-24 18:24:57 -0800

    added the animation in the shop section

--- app/sections/Shop.jsx
+++ app/sections/Shop.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+"use client"
+
+import React, { useRef } from 'react'
 import DisplayBox from '../components/DisplayBox'
+import { motion, useScroll, useTransform } from 'motion/react'
 
 const Shop = () => {
 
@@ -21,23 +24,42 @@ const Shop = () => {
     images: "/blueberry.jpg"
   }
 
+  const shopRef = useRef()
+  const { scrollYProgress } = useScroll({
+    target: shopRef,
+    offset: ["start end", "end start"]
+  })
+
+  const col1Y = useTransform(scrollYProgress, [0, 1], [0, -100]); // Moves up
+  const col2Y = useTransform(scrollYProgress, [0, 1], [0, 200]);  // Moves down
+  const col3Y = useTransform(scrollYProgress, [0, 1], [0, -300]); // Moves up slowly
+
   return (
-    <div className='mt-32 px-8 text-secondary'>
-      <div className='grid grid-cols-3 gap-x-6'>
-        <div className='col-start-1 col-span-1 flex flex-col gap-6 translate-y-12'>
+    <motion.div 
+    id='shop'
+    ref={shopRef}
+    className='pt-32 px-8 text-secondary'>
+      <div className='grid grid-cols-3 gap-x-6 pt-32'>
+        <motion.div 
+        style={{ y: col1Y }}
+        className='col-start-1 col-span-1 flex flex-col gap-6 translate-y-12'>
           <DisplayBox details={tiramisu}/>
           <DisplayBox details={strawshort}/>
-        </div>
-        <div className='col-start-2 col-span-1 flex flex-col gap-6 translate-y-48'>
-          <h3 className='-translate-y-32 font-EB text-walnut text-4xl font-medium'>OUR <span>FRESH</span> BAKED PRODUCTS</h3>
+        </motion.div>
+        <motion.div 
+        style={{ y: col2Y }}
+        className='col-start-2 col-span-1 flex flex-col gap-6 translate-y-48'>
+          <h3 className='-translate-y-24 font-EB text-walnut text-4xl font-medium'>OUR <span>FRESH</span> BAKED PRODUCTS</h3>
           <DisplayBox details={strawshort}/>
           <DisplayBox details={strawshort}/>
-        </div>
-        <div className='col-start-3 col-span-1 flex flex-col gap-6 translate-y-12'>
+        </motion.div>
+        <motion.div 
+        style={{ y: col3Y }}
+        className='col-start-3 col-span-1 flex flex-col gap-6 translate-y-12'>
           <DisplayBox details={blueberry}/>
-        </div>
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
