5699858b74fbf5b82a43c40656686eeb4634c38e
Jason Sugiharto <jasonliesugiharto@gmail.com>
2025-01-24 16:45:49 -0800

    changed the formatting for the shop

--- app/components/DisplayBox.jsx
+++ app/components/DisplayBox.jsx
@@ -4,32 +4,32 @@ import React, { useState } from 'react'
 import { motion } from  'motion/react'
 import Image from 'next/image'
 
-const DisplayBox = () => {
+const DisplayBox = ({details}) => {
 
-  const [hover, setHover] = useState(false)
+  const [hover, setHover] = useState()
   return (
     <div className='border-[1px] border-walnut font-Open' onMouseEnter={() => setHover(!hover)} onMouseLeave={() => setHover(!hover)}>
-      <h1 className='px-2 py-2 font-Paris text-5xl'>Tiramisu</h1>
+      <h1 className='px-2 py-2 font-Paris text-5xl'>{details.name}</h1>
       <hr className='border-walnut'/>
       <div className='flex overflow-hidden'>
         <motion.p 
-        initial={{x: 0}} animate={hover ? {x: "-100%"} : {x: 0}} transition={{duration: hover ? 10 : 0, ease: 'linear', repeat: hover ? Infinity : 0}}
+        initial={{x: 0}} animate={{x: "-100%"}} transition={{duration: 10, ease: 'linear', repeat: Infinity}}
         className='flex flex-shrink-0 justify-center items-center'>
-          {["test", "marquee", "for", "ingredients", "animation"].map((word, id) => (
+          {details.ingredients.map((word, id) => (
             <span key={id} className='pr-20 tracking-tighter text-xl'>{word}</span>
           ))}
         </motion.p>
         <motion.p 
-        initial={{x: 0}} animate={hover ? {x: "-100%"} : {}} transition={{duration: hover ? 10 : 0, ease: 'linear', repeat: hover ? Infinity : 0}}
+        initial={{x: 0}} animate={{x: "-100%"}} transition={{duration: 10, ease: 'linear', repeat: Infinity}}
         className='flex flex-shrink-0 justify-center items-center'>
-          {["test", "marquee", "for", "ingredients", "animation"].map((word, id) => (
+          {details.ingredients.map((word, id) => (
             <span key={id} className='pr-20 tracking-tighter text-xl'>{word}</span>
           ))}
         </motion.p>
       </div>
       <hr className='border-walnut'/>
       <div className='px-2 mt-6 pb-4'>
-        <motion.img animate={hover ? {scale: 1} : {scale: 0.8}} transition={{duration: 0.3, ease: "easeIn"}} className='rounded-2xl' src="/tiramisu.jpg" alt=""/>
+        <motion.img animate={hover ? {scale: 1} : {scale: 0.8}} transition={{duration: 0.3, ease: "easeIn"}} className='rounded-2xl' src={details.images} alt=""/>
         {/* <Image src="/tiramisu.jpg"  alt='tiramisu'/> */}
       </div>
     </div>
--- app/sections/Shop.jsx
+++ app/sections/Shop.jsx
@@ -2,20 +2,39 @@ import React from 'react'
 import DisplayBox from '../components/DisplayBox'
 
 const Shop = () => {
+
+  const tiramisu = {
+    name: "Tiramisu",
+    ingredients: ["test", "marquee", "for", "ingredients", "animation"],
+    images: "/tiramisu.jpg"
+  }
+
+  const strawshort = {
+    name: "Strawberry Shortcake",
+    ingredients: ["test", "marquee", "for", "ingredients", "animation"],
+    images: "/strawshort.jpg"
+  }
+
+  const blueberry = {
+    name : "Blueberry Shortcake",
+    ingredients: ["test", "marquee", "for", "ingredients", "animation"],
+    images: "/blueberry.jpg"
+  }
+
   return (
-    <div className='mt-32 px-8'>
-      <div className='grid grid-cols-6 gap-x-6'>
-        <div className='col-start-1 col-span-2'>
-          <DisplayBox/>
+    <div className='mt-32 px-8 text-secondary'>
+      <div className='grid grid-cols-3 gap-x-6'>
+        <div className='col-start-1 col-span-1 flex flex-col gap-6 translate-y-12'>
+          <DisplayBox details={tiramisu}/>
+          <DisplayBox details={strawshort}/>
         </div>
-        <div className='col-start-3 col-span-2'>
-          Display-2
+        <div className='col-start-2 col-span-1 flex flex-col gap-6 translate-y-48'>
+          <h3 className='-translate-y-32 font-EB text-walnut text-4xl font-medium'>OUR <span>FRESH</span> BAKED PRODUCTS</h3>
+          <DisplayBox details={strawshort}/>
+          <DisplayBox details={strawshort}/>
         </div>
-        <div className='col-start-5 col-span-2'>
-          Display-3
-        </div>
-        <div className='col-start-1 col-span-2'>
-          Display-4
+        <div className='col-start-3 col-span-1 flex flex-col gap-6 translate-y-12'>
+          <DisplayBox details={blueberry}/>
         </div>
       </div>
     </div>
