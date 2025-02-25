e314d89cf7b91cebe9bff8178e32e88b11aa32cc
Jason Sugiharto <jasonliesugiharto@gmail.com>
2025-02-24 21:43:40 -0800

    added react lenis

--- app/pages/online-order/page.jsx
+++ app/pages/online-order/page.jsx
@@ -1,18 +1,8 @@
 import CartDisplayBox from '@/app/_components/CartDisplayBox'
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
 import Link from 'next/link'
-import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { ReactLenis } from 'lenis/react'
 import React from 'react'
-import { NavBarScrollCart, NavBarScrollRouted } from '@/app/_components/Navbar';
+import { NavBarScrollCart } from '@/app/_components/Navbar';
 
 const page = () => {
 
@@ -33,68 +23,70 @@ const page = () => {
     {name: "Fudgy Brownies", desc: "A fudgy & decadent brownies with a rich chocolate flavor", price: 22, image: "/brown.jpg"}
   ]
   return (
-    <section className='flex flex-col'>
-      <NavBarScrollCart/>
-      <nav className='flex items-center justify-between pr-8 bg-soft'>
-        <div className='flex gap-3 items-center'>
-          <Link href={"/"}>
-            <img src="/logo-bw.png" alt="" className='md:w-[150px] w-[100px]'/>
-          </Link>
-          <h2 className='font-Corn md:text-[3rem] text-[1.5rem] font-semibold text-walnut'>Online Ordering</h2>
+    <ReactLenis root>
+      <section className='flex flex-col'>
+        <NavBarScrollCart/>
+        <nav className='flex items-center justify-between pr-8 bg-soft'>
+          <div className='flex gap-3 items-center'>
+            <Link href={"/"}>
+              <img src="/logo-bw.png" alt="" className='md:w-[150px] w-[100px]'/>
+            </Link>
+            <h2 className='font-Corn md:text-[3rem] text-[1.5rem] font-semibold text-walnut'>Online Ordering</h2>
+          </div>
+        </nav>
+        <div className='flex px-12 justify-between gap-4'>
+          <div className='flex flex-col w-full'>
+            <h3 className='font-Corn text-walnut md:text-[2.5rem] text-[2rem] font-medium'>Cakes</h3>
+            <div className='grid md:grid-cols-3 grid-cols-2 w-full mt-5 gap-4'>
+              <div className='col-span-1'>
+                <CartDisplayBox item={cakes[0]}/>
+              </div>
+              <div className='col-span-1'>
+                <CartDisplayBox item={cakes[1]}/>
+              </div>
+              <div className='col-span-1'>
+                <CartDisplayBox item={cakes[2]}/>
+              </div>
+              <div className='col-span-1'>
+                <CartDisplayBox item={cakes[3]}/>
+              </div>
+            </div>
+            {/* <div className='grid grid-cols-3 w-full mt-8'>
+              <div className='col-span-1'>
+                <CartDisplayBox item={cakes[3]}/>
+              </div>
+              <div className='col-span-1'>
+                &nbsp;
+              </div>
+              <div className='col-span-1'>
+                &nbsp;
+              </div>
+            </div> */}
+            <h3 className='font-Corn text-walnut md:text-[2.5rem] text-[2rem] font-medium mt-10'>Cream Puffs</h3>
+            <div className='grid md:grid-cols-3 grid-cols-2 w-full mt-8 gap-4'>
+              <div className='col-span-1'>
+                <CartDisplayBox item={puffs[0]}/>
+              </div>
+              <div className='col-span-1'>
+                <CartDisplayBox item={puffs[1]}/>
+              </div>
+              <div className='col-span-1'>
+                <CartDisplayBox item={puffs[2]}/>
+              </div>
+            </div>
+            <h3 className='font-Corn text-walnut md:text-[2.5rem] text-[2rem] font-medium mt-4'>Brownies</h3>
+            <div className='grid grid-cols-3 w-full mt-8 gap-4'>
+              <div className='col-span-1'>
+                <CartDisplayBox item={brownies[0]}/>
+              </div>
+            </div>
+          </div>
+        <div className='w-1/2 md:flex hidden'>
+          &nbsp;
         </div>
-      </nav>
-      <div className='flex px-12 justify-between gap-4'>
-        <div className='flex flex-col w-full'>
-          <h3 className='font-Corn text-walnut md:text-[2.5rem] text-[2rem] font-medium'>Cakes</h3>
-          <div className='grid md:grid-cols-3 grid-cols-2 w-full mt-5 gap-4'>
-            <div className='col-span-1'>
-              <CartDisplayBox item={cakes[0]}/>
-            </div>
-            <div className='col-span-1'>
-              <CartDisplayBox item={cakes[1]}/>
-            </div>
-            <div className='col-span-1'>
-              <CartDisplayBox item={cakes[2]}/>
-            </div>
-            <div className='col-span-1'>
-              <CartDisplayBox item={cakes[3]}/>
-            </div>
-          </div>
-          {/* <div className='grid grid-cols-3 w-full mt-8'>
-            <div className='col-span-1'>
-              <CartDisplayBox item={cakes[3]}/>
-            </div>
-            <div className='col-span-1'>
-              &nbsp;
-            </div>
-            <div className='col-span-1'>
-              &nbsp;
-            </div>
-          </div> */}
-          <h3 className='font-Corn text-walnut md:text-[2.5rem] text-[2rem] font-medium mt-10'>Cream Puffs</h3>
-          <div className='grid md:grid-cols-3 grid-cols-2 w-full mt-8 gap-4'>
-            <div className='col-span-1'>
-              <CartDisplayBox item={puffs[0]}/>
-            </div>
-            <div className='col-span-1'>
-              <CartDisplayBox item={puffs[1]}/>
-            </div>
-            <div className='col-span-1'>
-              <CartDisplayBox item={puffs[2]}/>
-            </div>
-          </div>
-          <h3 className='font-Corn text-walnut md:text-[2.5rem] text-[2rem] font-medium mt-4'>Brownies</h3>
-          <div className='grid grid-cols-3 w-full mt-8 gap-4'>
-            <div className='col-span-1'>
-              <CartDisplayBox item={brownies[0]}/>
-            </div>
-          </div>
         </div>
-       <div className='w-1/2 md:flex hidden'>
-        &nbsp;
-       </div>
-      </div>
-    </section>
+      </section>
+    </ReactLenis>
   )
 }
 
