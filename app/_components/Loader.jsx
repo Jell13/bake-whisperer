71c9eab48e534c51a431fbba67baeaf9d19325e5
Jason Sugiharto <jasonliesugiharto@gmail.com>
2025-09-30 21:33:38 -0700

    updated the matcha image for loader screen

--- app/_components/Loader.jsx
+++ app/_components/Loader.jsx
@@ -320,7 +320,7 @@ const GSAPLoader = ({ setLoading }) => {
           }}
         >
           <img 
-            src="/intro/mtch1.jpg" 
+            src="/intro/mtch-1.jpg" 
             alt="Cake 4"
             className="w-full h-full object-cover rounded-lg shadow-lg"
           />
